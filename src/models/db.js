797583f2a19d6ff7989c39c9bdd9ed55d67639c0
saad naikwade <naikwadesaad@gmail.com>
2025-09-07 22:35:19 +0530

    initial commit

---
+++ src/models/db.js
@@ -0,0 +1,18 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+let db;
+try {
+  db = await mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+  console.log("✅ MySQL Connected...");
+} catch (err) {
+  console.error("❌ Database connection failed:", err.message);
+}
+
+export default db;
